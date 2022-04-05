c96aceb79507e62a606d0c911f607f9ee655ede5
kylecarbonell <kylecarbonell13@gmail.com>
2022-04-04 22:28:57 -0700

    Working on search bar

--- Main.py
+++ Main.py
@@ -1,35 +1,35 @@
-from typing import KeysView
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
 import requests
 
 #Create needed objects
-browser = webdriver.Chrome(executable_path='C:\Program Files (x86)\Google\Chrome\Application\chrome.exe')
+path = Service('C:\Program Files (x86)\chromedriver.exe')
+browser = webdriver.Chrome(service=path)
 url = 'https://tracker.gg/valorant'
 browser.get(url)
 text = requests.get(url).text
 
+#Search Bar
+search = browser.find_elements(By.XPATH, '//*[@id="app"]/div[2]/div[2]/div/main/div[2]/div/div[1]/div/div/div[1]/div[2]/div[1]/form/input')
+search.send_keys('dazaichann#poopy')
+search.send_keys(Keys.RETURN)
+
+print(browser.page_source)
+time.sleep(50)
+
 #HTML Parser
 soup = BeautifulSoup(text, "html.parser")
-
-#Search bar
-searchBar = browser.find_element_by_xpath('//*[@id="app"]/div[2]/div[2]/div/main/div[2]/div/div[1]/div/div/div[1]/div[2]/div[1]/form')
-searchBar.send_keys("Tenz")
-searchBar.send_keys(Keys.RETURN)
-
-
 stats = soup.find_all('div', class_ = 'numbers')
-
-#Selenium: Find person
 
 
 #Print stats
-for stat in stats:
-    numbers = stat.find('span', class_="value").text
-    titles = stat.find('span', class_="name").text
-    print(f"{titles}: {numbers}")
-    print('')
-# kd = stats.find_all('')
-
+# for stat in stats:
+#     numbers = stat.find('span', class_="value").text
+#     titles = stat.find('span', class_="name").text
+#     print(f"{titles}: {numbers}")
+#     print('')
 
