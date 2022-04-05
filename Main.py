bd7c28d23b1406feecba550b7ed0875feb6e6a7e
Kyle Carbonell <kylecarbonell13@gmail.com>
2022-04-04 21:33:28 -0700

    Working on Selenium search bar

--- Main.py
+++ Main.py
@@ -1,14 +1,30 @@
+from typing import KeysView
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import requests
 
-text = requests.get('https://tracker.gg/valorant/profile/riot/SEN%20TenZ%230505/overview').text
+#Create needed objects
+browser = webdriver.Chrome(executable_path='C:\Program Files (x86)\Google\Chrome\Application\chrome.exe')
+url = 'https://tracker.gg/valorant'
+browser.get(url)
+text = requests.get(url).text
+
+#HTML Parser
 soup = BeautifulSoup(text, "html.parser")
+
+#Search bar
+searchBar = browser.find_element_by_xpath('//*[@id="app"]/div[2]/div[2]/div/main/div[2]/div/div[1]/div/div/div[1]/div[2]/div[1]/form')
+searchBar.send_keys("Tenz")
+searchBar.send_keys(Keys.RETURN)
+
 
 stats = soup.find_all('div', class_ = 'numbers')
 
-#K/D Stat
+#Selenium: Find person
 
 
+#Print stats
 for stat in stats:
     numbers = stat.find('span', class_="value").text
     titles = stat.find('span', class_="name").text
