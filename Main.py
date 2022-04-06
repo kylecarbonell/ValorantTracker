3aa60c1f12ca332c98e3f1a60cc58398f2340583
Kyle Carbonell <kylecarbonell13@gmail.com>
2022-04-06 14:53:14 -0700

    Working on the bot

--- Main.py
+++ Main.py
@@ -6,6 +6,9 @@ from selenium.webdriver.common.by import By
 import time
 import requests
 
+#Request Player
+user = input("Username and ID: ")
+
 #Create needed objects
 path = Service('C:\Program Files (x86)\chromedriver.exe')
 browser = webdriver.Chrome(service=path)
@@ -14,22 +17,23 @@ browser.get(url)
 
 #Search Bar
 search = browser.find_element(By.XPATH, '//*[@id="app"]/div[2]/div[2]/div/main/div[2]/div/div[1]/div/div/div[1]/div[2]/div[1]/form/input')
-search.send_keys('dazaichann#poopy')
+search.send_keys(user)
 search.send_keys(Keys.RETURN)
+time.sleep(3)
 
-print(browser.page_source)
-time.sleep(50)
+text = browser.page_source
+time.sleep(2)
 browser.quit()
 
 #HTML Parser
-# soup = BeautifulSoup(text, "html.parser")
-# stats = soup.find_all('div', class_ = 'numbers')
+soup = BeautifulSoup(text, "html.parser")
+stats = soup.find_all('div', class_ = 'numbers')
 
 
-# #Print stats
-# for stat in stats:
-#     numbers = stat.find('span', class_="value").text
-#     titles = stat.find('span', class_="name").text
-#     print(f"{titles}: {numbers}")
-#     print('')
+
+#Print stats
+for stat in stats:
+    numbers = stat.find('span', class_="value").text
+    titles = stat.find('span', class_="name").text
+    print(f"{titles}: {numbers}")
 
