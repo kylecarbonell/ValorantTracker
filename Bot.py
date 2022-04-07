c372780564ddf8c7f58dc2fda2e7d67ad9b65d91
kylecarbonell <kylecarbonell13@gmail.com>
2022-04-06 22:56:21 -0700

    Work on bot and additional Functions

---
+++ Bot.py
@@ -0,0 +1,10 @@
+from Main import Functions
+from discord.ext import commands
+
+p1 = Functions()
+print(p1.getStat("K/D Ratio"))
+print(p1.getStat("Most Kills (Match)"))
+
+#Make Bot
+#Functions to call get user and get Stat
+#Discord Bot channel
--- Main.py
+++ Main.py
@@ -1,39 +1,59 @@
+from mimetypes import init
+from pickle import NONE
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
-import requests
 
-#Request Player
-user = input("Username and ID: ")
+class Functions:
+    def __init__(self):
+        #Dictionary
+        self.dict = {
+            "K/D Ratio" : 0,
+            "Headshots" : 0,
+            "Wins" : 0,
+            "Kills" : 0,
+            "Aces" : 0,
+            "Most Kills (Match)" : 0
+        }
 
-#Create needed objects
-path = Service('C:\Program Files (x86)\chromedriver.exe')
-browser = webdriver.Chrome(service=path)
-url = 'https://tracker.gg/valorant'
-browser.get(url)
+    def getUser(self):
+        #Request Player
+        user = input("Username and ID: ")
 
-#Search Bar
-search = browser.find_element(By.XPATH, '//*[@id="app"]/div[2]/div[2]/div/main/div[2]/div/div[1]/div/div/div[1]/div[2]/div[1]/form/input')
-search.send_keys(user)
-search.send_keys(Keys.RETURN)
-time.sleep(3)
+        #Create needed objects
+        path = Service('C:\Program Files (x86)\chromedriver.exe')
+        browser = webdriver.Chrome(service=path)
+        url = 'https://tracker.gg/valorant'
+        browser.get(url)
 
-text = browser.page_source
-time.sleep(2)
-browser.quit()
+        #Search Bar
+        search = browser.find_element(By.XPATH, '//*[@id="app"]/div[2]/div[2]/div/main/div[2]/div/div[1]/div/div/div[1]/div[2]/div[1]/form/input')
+        search.send_keys(user)
+        search.send_keys(Keys.RETURN)
+        time.sleep(3)
 
-#HTML Parser
-soup = BeautifulSoup(text, "html.parser")
-stats = soup.find_all('div', class_ = 'numbers')
+        text = browser.page_source
+        browser.quit()
+
+        #HTML Parser
+        soup = BeautifulSoup(text, "html.parser")
+        stats = soup.find_all('div', class_ = 'numbers')
+        
+        #Print stats
+        for stat in stats:
+            numbers = stat.find('span', class_="value").text
+            titles = stat.find('span', class_="name").text
+            for i in self.dict:
+                if i == titles:
+                    self.dict[i] = numbers
+
+    def getStat(self, key):
+        return key + ": " + self.dict[key]
 
 
 
-#Print stats
-for stat in stats:
-    numbers = stat.find('span', class_="value").text
-    titles = stat.find('span', class_="name").text
-    print(f"{titles}: {numbers}")
-
+#Bot: /kd dazaichann
+#self.dict["K/D Ratio"] = your KD
